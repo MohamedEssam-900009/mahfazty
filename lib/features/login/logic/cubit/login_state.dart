6e06f5a424b1fec622d4cf5fc945dbb6ce78d812
Mohamed Essam <77198018+MohamedEssam-900009@users.noreply.github.com>
2024-02-24 15:39:14 +0200

    + Login Screen done

---
+++ lib/features/login/logic/cubit/login_state.dart
@@ -0,0 +1,10 @@
+
+abstract class LoginState {}
+
+final class LoginInitial extends LoginState {}
+
+final class LoginLoadingState extends LoginState {}
+
+final class LoginErrorState extends LoginState {}
+
+final class LoginSuccessState extends LoginState {}
---
+++ lib/my_app.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'core/routing/routes.dart';
+
+import 'core/routing/routing.dart';
+import 'features/login/ui/screens/login_screen.dart';
+
+class MyApp extends StatelessWidget {
+final Routing routing;
+
+  const MyApp({super.key, required this.routing});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(390, 844),
+      minTextAdapt: true,
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Mahfazty',
+        theme: ThemeData(useMaterial3: false),
+        home: const LoginScreen(),
+        initialRoute: Routes.login,
+        onGenerateRoute: routing.onGenerateRoute,
+      ),
+    );
+  }
+}
